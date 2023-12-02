02cad1ee0f1498b6172889e4c968dbe882d6351d
lijiang <2953646794@qq.com>
2023-12-02 22:19:59 +0800

    001

---
+++ src/main/java/org/example/github_action/GithubActionApplication.java
@@ -0,0 +1,13 @@
+package org.example.github_action;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/github_action/GithubActionApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.github_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
